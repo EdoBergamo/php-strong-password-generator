71786a27b3e25eb9678560446b016dd0263e7019
Edo <hello@edoardo-hb.it>
2024-01-26 15:03:47 +0100

    feat: implemented initial logic

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <h1>Genera Password</h1>
+
+  <form method="GET">
+    <label for="length">Lunghezza Password</label>
+    <input type="number" id="length" name="length" min="8" max="32" />
+    <button type="submit">Genera Password</button>
+  </form>
+
+  <?php
+    function generatePassword($length) {
+      $char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@';
+      return substr(str_shuffle($char), 0, $length);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == "GET") {
+      $length = $_GET['length'];
+
+      $password = generatePassword($length);
+
+      echo "<p>Password: {$password}</p>";
+    }
+  ?>
 </body>
 </html>
